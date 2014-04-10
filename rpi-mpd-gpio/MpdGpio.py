6c12c69fe6b5e2ce11541adacf1b12ac5146c1ea
denniss17 <dennisschroer@hotmail.com>
2014-04-10 22:55:38 +0200

    Renamings; Screen update

--- rpi-mpd-gpio/MpdGpio.py
+++ rpi-mpd-gpio/MpdGpio.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import socket
 from mpd import MPDClient, ConnectionError
 from Timer import Timer
-from Screen import *
+from Screen import Screen
 
 ''' MPD settings '''
 MPD_ADDRESS = "localhost"
@@ -21,9 +21,11 @@ POLLING_INTERVAL = 0.5 # Time (in s) between mpd status pollings
 SCREEN_UPDATE_INTERVAL = 1 # Time (in s) between screen updates
 BUTTON_BOUNCETIME = 200 # Time (in ms) as used for the software bouncetime of the pins
 
+
+
 '''
 Value examples
-self.status =
+self.status.mpd =
 {
     'songid': '260', 
     'playlistlength': '194', 
@@ -45,7 +47,7 @@ self.status =
     'nextsongid': '383', 
     'audio': '44100:24:2'
 }
-self.song =
+self.status.song =
 {
     'album': 'All The Little Lights', 
     'artist': 'Passenger', 
@@ -64,14 +66,23 @@ self.song =
 
 
 class MpdGpio:
+    ''' Constants. Don't change! '''
+    STATE_STARTUP = 0;
+    STATE_NORMAL = 1;
+    
     mpd = None
     screen = None
     timer = None
-    status = None
-    song = None
+    status = dict()
+    observers = []
     timer = None
     
+    # Constructor
     def __init__(self):
+        # Set state
+        self.status['state'] = self.STATE_STARTUP
+        
+        # Setup MPD client
         self.mpd = MPDClient()
         self.mpd.timeout = 10  # network timeout in seconds (floats allowed), default: None
         self.mpd.idletimeout = None
@@ -91,18 +102,25 @@ class MpdGpio:
         
         # Start LCD
         self.screen = Screen(self)
+        self.screen.onStatusChange()
+        
         print(self.getIpAddress())
         
         # Start timer
         self.timer = Timer()
-        self.timer.addRepeatingTask(POLLING_INTERVAL, self.mpdUpdateStatus)
-        self.timer.addRepeatingTask(SCREEN_UPDATE_INTERVAL, self.screen.tick)
+        self.timer.addRepeatingTask(POLLING_INTERVAL, self.updateMpdStatus)
+        #self.timer.addRepeatingTask(SCREEN_UPDATE_INTERVAL, self.screen.tick)
         self.timer.start()
         
-        
+    # Getters
+    ''' Get the GPIO interface '''
     def getGPIO(self):
-        return self.GPIO
-        
+        return self.GPIO  
+    
+    def getStatus(self):
+        return self.status      
+    
+    ''' Get the current IP address or None if it doesn't have one '''
     def getIpAddress(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -111,17 +129,25 @@ class MpdGpio:
         except Error as e:
             return None
     
-    def mpdConnect(self):
-        self.mpd.connect(MPD_ADDRESS, MPD_PORT)
+    # Methods
+    ''' Add a state observer to the list of observers. The observer had to have an onStatusChange() callback '''
+    def addObserver(self, observer):
+        self.observers.append(observer)
+    
+    ''' Notify the observers that the status has changed '''
+    def notifyObservers(self):
+        for observer in self.observers:
+            observer.onStatusChange()
         
-    def mpdUpdateStatus(self):
+    def updateMpdStatus(self):
         done = False
         while not done:
             try:
-                self.status = self.mpd.status()
-                self.song = self.mpd.currentsong()
-                if self.screen.getState() == STATE_STARTUP:
-                    self.screen.setState(STATE_NORMAL)
+                self.status['mpd'] = self.mpd.status()
+                self.status['song'] = self.mpd.currentsong()
+                if self.status['state'] == self.STATE_STARTUP:
+                    self.status['state'] = self.STATE_NORMAL
+                self.notifyObservers()
                 done = True
                 #print(str(self.status))
                 #print(str(self.song))
@@ -129,41 +155,54 @@ class MpdGpio:
                 print(str(e))
                 self.mpdConnect()
             
-                
-    
+    def mpdConnect(self):
+        self.mpd.connect(MPD_ADDRESS, MPD_PORT)         
     
     def pauseOrPlay(self, channel):
-        if 'state' in self.status:
-            if self.status['state'] == 'play':
-                self.mpd.pause(1)
-                self.screen.setState(STATE_PAUSE)
-                print('Pause')
+        if 'state' in self.status['mpd']:
+            if self.status['mpd']['state'] == 'play':
+                try:
+                    self.mpd.pause(1)
+                    self.status['mpd']['state'] = 'pause'
+                    print('Pause')
+                except: pass
             else:
-                self.mpd.pause(0)
-                self.screen.setState(STATE_NORMAL)
-                print('Play')
+                try: 
+                    self.mpd.pause(0)
+                    self.status['mpd']['state'] = 'play'
+                    print('Play')
+                except: pass
+            self.notifyObservers()
         
     def volumeUp(self, channel):
         if 'volume' in self.status:
             try:
-                vol = int(self.status['volume'])
+                vol = int(self.status['mpd']['volume'])
             except:
                 vol = 0
                 
             if vol < 100:
-                self.mpd.setvol(vol + 1)
-            print('Volume Up: ' + str(vol + 1))
+                try:
+                    self.mpd.setvol(vol + 1)
+                    self.status['mpd']['volume'] = str(vol+1)
+                    self.notifyObservers()
+                    print('Volume Down: ' + str(vol + 1))
+                except: pass # Sometimes ProtocolError occurs
         
     def volumeDown(self, channel):
         if 'volume' in self.status:
             try:
-                vol = int(self.status['volume'])
+                vol = int(self.status['mpd']['volume'])
             except:
                 vol = 100
-                
+               
             if vol > 0:
-                self.mpd.setvol(vol - 1)
-            print('Volume Down: ' + str(vol - 1))
+                try:
+                    self.mpd.setvol(vol - 1)
+                    self.status['mpd']['volume'] = str(vol-1)
+                    self.notifyObservers()
+                    print('Volume Down: ' + str(vol - 1))
+                except: pass # Sometimes ProtocolError occurs   
             
 mpdgpio = MpdGpio()
         
--- rpi-mpd-gpio/Screen.py
+++ rpi-mpd-gpio/Screen.py
@@ -1,101 +1,130 @@
 from LCD import LCD
 from threading import Lock
 
-STATE_STARTUP = 0;
-STATE_NORMAL = 1;
-STATE_PAUSE = 2;
-
 TEXT_START = 'Starting...'
 TEXT_PAUSE = '|| Pause        '
+TEXT_NO_SONG = 'No song playing!'
+
+ERROR_NO_MPD_STATE = 'E: No MPD state'
 
 class Screen:
     """ Mainly the 'view' of this application. Controls the LCD """
     mpdgpio = None
-    state = None
     lock = None
-    substate = None
-    oldState = None
-    displayedSong = None
+    substate = 0
+    currentStatus = None
     lcd = None
     
     def __init__(self, mpdgpio):
         self.lock = Lock()
         self.mpdgpio = mpdgpio
-        self.state = STATE_STARTUP
+        self.mpdgpio.addObserver(self)
         self.GPIO = self.mpdgpio.getGPIO()
         # Use other layout (GPIO.BOARD, so pin number change)
         self.lcd = LCD(pin_rs=22, pin_e=18, pins_db=[16, 11, 13, 15], GPIO=self.GPIO)
         #self.lcd = LCD(GPIO=self.GPIO)
         self.lcd.begin(16,2)
         
-    def getState(self):
-        return self.state
-        
-    def setState(self, state):
-        self.oldState = self.state
-        self.state = state
+    def onStatusChange(self):
+        print('onStatusChange')
         self.tick()
-        self.oldState = self.state
+        self.currentStatus = self.mpdgpio.getStatus()
         
     def padString(self, string):
         return string + ((16-len(string)) * ' ')
     
+    ''' Format a number of seconds to a time string (ex: 115 becomes "1:55") '''
     def toTime(self, number):
         return '%d:%02d' % (int(float(number)) / 60, int(float(number)) % 60)
         
     def tick(self):
+        print('tick')
         # Use lock to avoid concurrent modification
         # Because the state can change in another thread (button callback thread)
         self.lock.acquire()
         try:
-            if self.state==STATE_STARTUP:
+            if self.mpdgpio.getStatus()['state']==self.mpdgpio.STATE_STARTUP:
                 self.tickStartup()
-            elif self.state==STATE_NORMAL:
+            elif self.mpdgpio.getStatus()['state']==self.mpdgpio.STATE_NORMAL:
                 self.tickNormal()
-            elif self.state==STATE_PAUSE:
-                self.tickPause()
         finally:
             self.lock.release()
             
     def tickStartup(self):
-        if self.oldState != STATE_STARTUP:
+        if self.currentStatus != None and self.currentStatus['state'] != STATE_STARTUP:
             self.lcd.clear()
             self.lcd.message(TEXT_START)
-            
-    def tickPause(self):
-        if self.oldState != STATE_PAUSE:
-            self.lcd.setCursor(0,0)
-            self.lcd.message(TEXT_PAUSE)
                 
     def tickNormal(self):
-        if self.displayedSong != self.mpdgpio.song:
+        status = self.mpdgpio.getStatus()
+        if 'song' not in self.currentStatus or self.currentStatus['song'] != status['song']:
             # New song
-            self.displayedSong = self.mpdgpio.song
             # COULD BE NONE!
             self.lcd.clear()
             self.substate = 0
+            
+        if 'mpd' not in status or 'state' not in status['mpd']:
+            self.lcd.message(ERROR_NO_MPD_STATE)
+            return
         
-        if self.displayedSong != None and 'artist' in self.displayedSong and 'title' in self.displayedSong:
+        if status['song'] != None and 'artist' in status['song'] and 'title' in status['song']:
             # Yay! there is a song playing :)
             self.lcd.setCursor(0,0)
             if self.substate==0:
                 # Show artist
-                self.lcd.message(self.padString(self.displayedSong['artist'][:16]))
-            elif self.substate==2:
+                self.lcd.message(self.padString(status['song']['artist'][:16]))
+            elif self.substate==4:
                 # Show title
-                self.lcd.message(self.padString(self.displayedSong['title'][:16]))
-            self.substate = (self.substate + 1) % 4
+                self.lcd.message(self.padString(status['song']['title'][:16]))
+            self.substate = (self.substate + 1) % 8
                 
             # Show progress on second line
-            self.lcd.setCursor(0,1)
-            if 'elapsed' in self.mpdgpio.status and 'time' in self.displayedSong:
-                self.lcd.message('%-8s%8s' % (self.toTime(self.mpdgpio.status['elapsed']), self.toTime(self.displayedSong['time'])))
+            self.lcd.setCursor(2,1)
+            if 'elapsed' in status['mpd'] and 'time' in status['song']:
+                self.lcd.message('%-7s%7s' % (self.toTime(status['mpd']['elapsed']), self.toTime(status['song']['time'])))
         else:
             # No song currently playing
-            self.lcd.message('No song playing!')
+            self.lcd.message(TEXT_NO_SONG)
+            
+        if status['mpd']['state'] == 'stop':
+            self.lcd.setCursor(0,1)
+            self.lcd.message('S')
+        elif status['mpd']['state'] == 'pause':
+            self.lcd.setCursor(0,1)
+            self.lcd.message('P')
+        elif status['mpd']['state'] == 'play':
+            self.lcd.setCursor(0,1)
+            self.lcd.message('>') 
+       
         
-        #self.lcd.message('.........0.........1.........2.........3.........4.........5.........6.........7')
-        #if 'artist' in self.mpdgpio.song and 'title' in self.mpdgpio.song:
+       #if status['mpd']['state'] == 'stop':
+       #     self.lcd.setCursor(0,0)
+       #     self.lcd.message(TEXT_NO_SONG)
+       # elif status['mpd']['state'] == 'pause':
+       #     self.lcd.setCursor(0,0)
+       #     self.lcd.message(TEXT_PAUSE)
+       # elif status['mpd']['state'] == 'play':
+       #     if status['song'] != None and 'artist' in status['song'] and 'title' in status['song']:
+       #         # Yay! there is a song playing :)
+       #         self.lcd.setCursor(0,0)
+       #         if self.substate==0:
+       #             # Show artist
+       #             self.lcd.message(self.padString(status['song']['artist'][:16]))
+       #         elif self.substate==2:
+       #             # Show title
+       #             self.lcd.message(self.padString(status['song']['title'][:16]))
+       #         self.substate = (self.substate + 1) % 4
+       #             
+       #         # Show progress on second line
+       #         self.lcd.setCursor(0,1)
+       #         if 'elapsed' in status['mpd'] and 'time' in status['song']:
+       #             self.lcd.message('%-8s%8s' % (self.toTime(status['mpd']['elapsed']), self.toTime(status['song']['time'])))
+       #     else:
+       #         # No song currently playing
+       #         self.lcd.message(TEXT_NO_SONG)
+       # '''
+       # #self.lcd.message('.........0.........1.........2.........3.........4.........5.........6.........7')
+       #if 'artist' in self.mpdgpio.song and 'title' in self.mpdgpio.song:
         #    self.lcd.message((self.mpdgpio.song['artist'] + '-' + self.mpdgpio.song['title'])[:40])
            # pass
         #self.lcd.clear()
